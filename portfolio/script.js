303728df365a9c5f3fcc5230a365da3027799f1f
Mahesh-m-s <777mahesh.m.s@gmail.com>
2026-03-12 17:58:05 +0800

    updated code

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,18 +1,73 @@
-const toggle = document.getElementById("modeToggle");
+/* typing animation */
 
-toggle.addEventListener("click", () => {
+const words = [
+"I am a Web Developer",
+"I Love Building UI",
+"I Solve DSA Problems"
+];
 
-document.body.classList.toggle("dark");
+let wordIndex = 0;
+let charIndex = 0;
 
-if(document.body.classList.contains("dark")){
-toggle.textContent = "☀️";
-document.body.style.background = "#121212";
-document.body.style.color = "white";
+function typeEffect(){
+
+let currentWord = words[wordIndex];
+
+document.getElementById("typing").textContent =
+currentWord.substring(0, charIndex);
+
+charIndex++;
+
+if(charIndex > currentWord.length){
+
+wordIndex++;
+
+charIndex = 0;
+
+if(wordIndex >= words.length){
+wordIndex = 0;
 }
-else{
-toggle.textContent = "🌙";
-document.body.style.background = "#f5f5f5";
-document.body.style.color = "#333";
+
+setTimeout(typeEffect,1000);
+
+}else{
+setTimeout(typeEffect,100);
+}
+
+}
+
+typeEffect();
+
+
+/* particles background */
+
+particlesJS("particles-js",{
+
+particles:{
+
+number:{
+value:80
+},
+
+size:{
+value:3
+},
+
+color:{
+value:"#00e5ff"
+},
+
+line_linked:{
+enable:true,
+distance:150,
+color:"#00e5ff",
+opacity:0.4
+},
+
+move:{
+speed:2
+}
+
 }
 
 });
